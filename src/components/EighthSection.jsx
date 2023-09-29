5abdda1969c6871415a9100393ac996bf1f593c1
mj_developer <140232119+MJ-Developer201@users.noreply.github.com>
2023-09-29 12:41:05 -0400

    Updated Missions Statement

--- src/components/EighthSection.jsx
+++ src/components/EighthSection.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 export default function EighthSection() {
   const divVariants = {
@@ -13,7 +13,7 @@ export default function EighthSection() {
       delay: 0.3,
       duration: 1.8,
     },
-  }
+  };
 
   return (
     <motion.div
@@ -21,22 +21,30 @@ export default function EighthSection() {
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.5, duration: 1.5 }}
     >
-      <div id='about' style={{ maxWidth: '1100px', padding: '3rem' }}>
-        <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold', marginBottom: '1rem' }}>
+      <div id="about" style={{ maxWidth: "1100px", padding: "3rem" }}>
+        <h1
+          style={{
+            fontSize: "2.5rem",
+            fontWeight: "bold",
+            marginBottom: "1rem",
+          }}
+        >
           Your Car Deserves the Best
         </h1>
-        <p style={{ lineHeight: '2rem', fontSize: '1.2rem' }}>
-          At [Your Car Detailing Company Name], we're passionate about providing top-notch car detailing
-          services. We offer a wide range of services to ensure your vehicle looks its best, from exterior
-          shine to interior cleanliness. Our team of skilled detailers takes pride in their work and uses the
-          latest techniques and products to exceed your expectations.
+        <p style={{ lineHeight: "2rem", fontSize: "1.2rem" }}>
+          At Dire Ride, we're passionate about providing top-notch car detailing
+          services. We offer a wide range of services to ensure your vehicle
+          looks its best, from exterior shine to interior cleanliness. Our team
+          of skilled detailers takes pride in their work and uses the latest
+          techniques and products to exceed your expectations.
         </p>
-        <p style={{ lineHeight: '2rem', fontSize: '1.2rem' }}>
-          When you choose us, you're choosing excellence and a commitment to your car's beauty and longevity.
-          Give us a call or visit us for a consultation today. Let's discuss how we can enhance the appearance
+        <p style={{ lineHeight: "2rem", fontSize: "1.2rem" }}>
+          When you choose us, you're choosing excellence and a commitment to
+          your car's beauty and longevity. Give us a call or visit us for a
+          consultation today. Let's discuss how we can enhance the appearance
           and health of your vehicle. Your car deserves the best, and so do you.
         </p>
       </div>
     </motion.div>
-  )
+  );
 }
